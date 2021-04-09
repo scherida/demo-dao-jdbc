242a7ea810a30d68dcce3c62d688354982c6ddd8
scherida rayane <scheridarayane@gmail.com>
2021-04-09 15:55:52 -0300

    Seller entity class

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -1,11 +1,17 @@
 package apllication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller saller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.00, obj);
+		
+		System.out.println(saller);
 	}
 }
