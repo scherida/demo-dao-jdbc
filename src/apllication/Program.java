874eeb29c8703a3a219827090edd25e79e46c129
scherida rayane <scheridarayane@gmail.com>
2021-04-09 13:18:41 -0300

    Depasrtment entity class

---
+++ src/apllication/Program.java
@@ -0,0 +1,11 @@
+package apllication;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+	}
+}
