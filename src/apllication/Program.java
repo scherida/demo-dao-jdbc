472d242b3ba72c5483adbfbc5553538badb1f93f
scherida rayane <scheridarayane@gmail.com>
2021-04-10 15:52:45 -0300

    Reusing instantiation

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -12,9 +12,11 @@ public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
 		
-		Seller saller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.00, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("====== Teste 1: seller findById ======");
+		
+		Seller seller = sellerDao.findById(3)
 		System.out.println(saller);
 	}
 }
